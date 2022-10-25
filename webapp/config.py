26fd4f56fa1bab4f1992213ea7481beb90663b90
Viktor Zagorenko <vzagorenko@bk.ru>
2022-10-25 18:11:43 +0300

    Добавили blueprint в проект, меню и декоратор admin_required

--- webapp/config.py
+++ webapp/config.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -7,3 +8,4 @@ WEATHER_API_KEY = '254b288df093406092c141709221809'
 WEATHER_URL = 'http://api.worldweatheronline.com/premium/v1/weather.ashx'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db')
 SECRET_KEY = "jsdhdkgrfsahcjkh34843hwedjljg459"
+REMEMBER_COOKIE_DURATION = timedelta(days=5)
--- webapp/user/models.py
+++ webapp/user/models.py
@@ -1,18 +1,7 @@
 from flask_login import UserMixin
-from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
-db = SQLAlchemy()
-
-class News(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
-    url = db.Column(db.String, unique=True, nullable=False)
-    published = db.Column(db.DateTime, nullable=False)
-    text = db.Column(db.Text, nullable=True)
-
-    def __repr__(self):
-        return '<News {} {}>'.format(self.title, self.url)
+from webapp.model import db
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -26,7 +15,9 @@ class User(db.Model, UserMixin):
     def check_password(self, password):
         return check_password_hash(self.password, password)
 
+    @property
+    def is_admin(self):
+        return self.role == 'admin'
+    
     def __repr__(self):
         return '<User {}>'.format(self.username)
-
-    
